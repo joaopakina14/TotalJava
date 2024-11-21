9c8d43104e6603907a47d9628f97c8d790f42784
João André Pimentel <pakina@live.com.pt>
2024-11-21 15:25:19 +0000

    Update Ex_05.java

--- TotalJava/src/Ficha6FunçõesMétProcedimentos/FichaPratica06/Ex_05.java
+++ TotalJava/src/Ficha6FunçõesMétProcedimentos/FichaPratica06/Ex_05.java
@@ -6,6 +6,15 @@ import static FichaPratica06.Ex_02.lerInteiroPositivo;
 
 public class Ex_05 {
 
+    //5. O exercício deve ser resolvido na mesma classe (por exemplo: Ex_05), deve acrescentar as funções conforme 
+//solicitado. No início não conseguirá testar se as funções funcionam ou não (uma vez que não é pedido para 
+//desenvolver nada no main), considere fazer o exercício 6 em concorrente para conseguir perceber se está 
+//tudo como esperado. 
+//a) Escreva uma função que recebe um array de inteiros como parâmetro e retorna o maior valor do array. 
+//Exemplo de Código:
+//b) Escreva uma função que recebe um array de inteiros como parâmetro e retorna o menor valor do array. 
+//c) Escreva uma função que recebe um array de inteiros como parâmetro e retorna se o array está crescente. 
+
     public static int[] preencherVetor(int tamanho){
 
         Scanner input = new Scanner(System.in);
